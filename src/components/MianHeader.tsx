f33c041d181211ed8b5d4d7b5b8aff106d1f1ed6
Rohama Majeed <rohamamajeed3@gmail.com>
2025-07-13 19:25:01 -0700

    change

--- src/components/MianHeader.tsx
+++ src/components/MianHeader.tsx
@@ -47,7 +47,7 @@ const MianHeader = () => {
 
   return (
     <>
-      <div className="hidden md:flex  justify-between z-50 p-3">
+      <div className="hidden md:flex  justify-between z-50 p-3 bg-[#1a1a1a]">
          <h1 className="text-2xl font-bold bg-gradient-to-r from-[#9f70fd] via-[#c084fc] to-[#ff6ec7] bg-clip-text text-transparent">
           Romi.Dev
         </h1>
